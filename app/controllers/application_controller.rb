518af01599bfc36f1433c4f9524eee11339692f3
Takai akira <z22.takaiakira@gmail.com>
2018-02-20 13:19:05 +0900

    Delete model create controller rooting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
 end
