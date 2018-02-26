fb9946b562bd25384e3aa5ced65eb86d0394f957
Takai akira <z22.takaiakira@gmail.com>
2018-02-26 17:30:04 +0900

    Correction
    1.無駄な余白の削除
    2.閉じタグの位置の修正
    3.a要素を link メソッドに書き換え
    4.git.ignoreの修正
    5.ハッシュロケットの書き換え

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,4 +4,3 @@ class MessagesController < ApplicationController
    end
 
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'chatspace' => 'messages#index'
+  root to: 'messages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
