e1b92a7455beb09d09ff71f877fdb7f628a33793
Takai akira <z22.takaiakira@gmail.com>
2018-02-27 12:10:39 +0900

    Devise install
    Devise ジェムのインストール
    Diviseのビューファイルの作成
    Before actionの定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
   config.generators do |g|
       g.javascripts false
