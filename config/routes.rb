8d2b220bab7b8d3cb4d83c81f55d8ef8a97b2382
Takai akira <z22.takaiakira@gmail.com>
2018-02-28 17:06:08 +0900

    Add group sidebar
    グループサイドバーのビューを追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :users, only: [:update, :edit]
+  resources :users, only: [:index, :update, :edit]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
