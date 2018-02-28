8e0f82655f07890fcc0530f67827b611497b6c91
Takai akira <z22.takaiakira@gmail.com>
2018-02-28 12:23:22 +0900

    Creat group controller and model
    groupsコントローラーの作成
    ・Groupテーブルの作成
    ・goup_userテーブルの作成
    ・Modelのアソシエーション
    ・groupsコントローラーの定義

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,15 @@
+class GroupsController < ApplicationController
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:update, :edit]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
