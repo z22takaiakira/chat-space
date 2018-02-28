6ffe538414c263fd13569994f7820d6d9ab11fd5
Takai akira <z22.takaiakira@gmail.com>
2018-02-28 11:18:40 +0900

    Add devise function
    編集画面と、完了したことのフラッシュを表示する

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
+
