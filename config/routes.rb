a734c8e351676eb414557f4a9dfb6c4cbac02de0
Takai akira <z22.takaiakira@gmail.com>
2018-02-28 11:46:19 +0900

    Improve rooting
    rootingでresouceをresoucesに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resource :users, only: [:update, :edit]
+  resources :users, only: [:update, :edit]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
