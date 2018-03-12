ab4f3e412717af8b09a074ae6ab81b8eb39e2151
Takai akira <z22.takaiakira@gmail.com>
2018-03-13 08:42:31 +0900

    define controller and juilder and change haml
    コントローラーの定義
    htmlをhamlに変更
    json形式で送るためのjbuilderの変数を定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
+    end
+
+  end
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
---
+++ app/views/users/index.json.jbuilder
@@ -0,0 +1,3 @@
+json.array! @users do |user|
+  json.name user.name
+  json.id user.id
