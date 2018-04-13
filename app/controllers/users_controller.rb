0e0e335da3a958170437ff5a8e7b67b2044430b5
Takai akira <z22.takaiakira@gmail.com>
2018-04-13 16:39:48 +0900

    Reply to review
    レビューで指摘のあった箇所の修正
    1.ハッシュロケットの書き替え
    2.current_userをデフォルトで表示するように
    3.不要な記述render son:@usersの削除
    4.current_userを検索対象から除外する。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id:current_user)
     respond_to do |format|
       format.html
-      format.json { render json: @users }
+      format.json
     end
   end
 
